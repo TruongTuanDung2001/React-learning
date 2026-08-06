d16f810212b4b0c07417fcf02b2a158c23d94f73
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-08-06 11:57:13 +0700

    Handle function getProductById() by params, useState, useEffect, fetch api

--- src/components/ProductDetailPage.jsx
+++ src/components/ProductDetailPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function ProductDetailPage() {
@@ -14,7 +15,47 @@ export default function ProductDetailPage() {
     */
   const { id } = useParams();
   // console.log("detail page");
-  // console.log(id); là id
+  //   console.log(id);
   // console.log(typeof id); string
-  return <h1>This is product detail page</h1>;
+  const API_URL = `http://localhost:3001/products`;
+  const [dataProduct, setDataProduct] = useState(null); // vì lấy 1 sản phẩm nên nó là 1 object hoặc null cho cùng kiểu dữ liệu
+  const [loading, setLoading] = useState(true);
+
+  async function getProductById(id) {
+    try {
+      setLoading(true); // để khi đổi dữ liệu thì vẫn còn loading
+      const response = await fetch(`${API_URL}/${id}`, {
+        method: "GET",
+      });
+      if (!response.ok) throw new Error("Get data product fails");
+      const data = await response.json();
+      console.log(data);
+      setDataProduct(data);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  // loading data local
+  useEffect(() => {
+    getProductById(id);
+  }, [id]); // đặt là id vì nếu đang ở sp1 mà chuyển sang sp2 thì id thay đổi sẽ chạy lại getProductById(id);
+
+  //return
+  return (
+    <div className="productDetail">
+      {loading ? (
+        <h3>Loading</h3>
+      ) : (
+        <div>
+          <h1>{dataProduct.name}</h1>
+          <p>Category: {dataProduct.category}</p>
+          <p>Price: {dataProduct.price}</p>
+          <p>Stock: {dataProduct.stock}</p>
+        </div>
+      )}
+    </div>
+  );
 }
