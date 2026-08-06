97c31a969cea5b426c315fb7ef611374ab911e34
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-08-06 13:22:33 +0700

    Set up environment for Nested Routes(outlet)

---
+++ src/pages/AdminLayout.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+export default function AdminLayoutPage(){
+    return (
+        <div className="adminLayout">
+            <Navbar/>
+
+            {/* outlet */}
+        </div>
+    )
+}
