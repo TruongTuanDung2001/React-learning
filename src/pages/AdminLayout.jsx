be8514240b20025e1cb2dc6e5f682c8faa99f1b1
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-08-06 14:18:58 +0700

    Fix navbar for admin page or user page

--- src/pages/AdminLayout.jsx
+++ src/pages/AdminLayout.jsx
@@ -4,7 +4,8 @@ import Navbar from "../components/Navbar";
 export default function AdminLayoutPage(){
     return (
         <div className="adminLayout">
-            {/* <Navbar/> */}
+            {/* <Navbar/> */} 
+            {/* cái chỗ navbar này thì: mỗi trang như user hay admin sẽ có 1 cái navbar riêng. Nên sau này có thể thay đổi chỗ navbar là của thiết kế riêng navbar cho từng trang sử dụng nó */}
 
             {/* outlet */}
             <Outlet/>
